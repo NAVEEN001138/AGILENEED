a69f0be4a41aea35019fcb46b0a348b477a0508c
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2025-12-15 12:25:47 +0530

    next

---
+++ need.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<stdlib.h>
+#define SIZE 100
+
+int stack[SIZE];
+int top = -1;
+
+void push(int value) {
+    if(top == SIZE - 1) {
+        printf("Stack Overflow\n");
+    } else {
+        stack[++top] = value;
+    }
+}
